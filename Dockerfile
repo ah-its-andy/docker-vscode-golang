b0a3a7f37555f19f2709873c045c775ba428373d
andy <mykge@outlook.com>
2021-04-29 16:57:33 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,15 @@ ARG GO_PROXY=https://goproxy.cn,direct
 
 ADD $GO_BIN_URL /etc/golang/golang.tar.gz
 
-RUN tar -xzvf /etc/golang/golang.tar.gz -C /etc/golang \
+RUN tar -xzf /etc/golang/golang.tar.gz -C /etc/golang \
 && rm /etc/golang/golang.tar.gz \
 && ln -s /etc/golang/go/bin/go /usr/bin/go
 
-RUN apt-get update \
-&& apt-get install -y openssh-server ca-certificates git\
-&& apt-get clean
+RUN apt-get update > /dev/null\
+&& apt-get install -y -q git\
+&& apt-get clean \
+&& apt-get autoclean \
+&& apt-get autoremove
 
 RUN /usr/bin/go env -w GOPROXY=$GO_PROXY \
 && /usr/bin/go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs \
@@ -22,5 +24,4 @@ RUN /usr/bin/go env -w GOPROXY=$GO_PROXY \
 && /usr/bin/go get -v github.com/haya14busa/goplay/cmd/goplay \
 && /usr/bin/go get -v github.com/go-delve/delve/cmd/dlv
 
-ENTRYPOINT /etc/init.d/ssh start -D
 
--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 GO_VERSIONS=("go1.16.3" "go1.16.2" "go1.16.1" "go1.16" "go1.15.11" "go1.15.10" "go1.15.9" "go1.15.8" "go1.15.7" "go1.15.6" "go1.15.5" "go1.15.4" "go1.15.3" "go1.15.2" "go1.15.1" "go1.15")
-ARCHS=("amd64" "arm64")
+ARCHS=("amd64")
 for((i=0;i<${#GO_VERSIONS[@]};i++));
 do
-   for((a=;a<${#ARCHS[@]};a++));
+   for((a=0;a<${#ARCHS[@]};a++));
    do
-      docker build ./ --build-args GO_BIN_URL=https://golang.google.cn/dl/${GO_VERSIONS[i]}.linux-${ARCHS[a]}.tar.gz -t standardcore/vscode-golang:${GO_VERSIONS[i]}-${ARCHS[a]}
+      echo "========== standardcore/vscode-golang:${GO_VERSIONS[i]}-${ARCHS[a]}-$1 =========="
+      echo "=============================================================================="
+      docker build ./ --build-arg GO_BIN_URL=https://golang.google.cn/dl/${GO_VERSIONS[i]}.linux-${ARCHS[a]}.tar.gz -t standardcore/vscode-golang:${GO_VERSIONS[i]}-${ARCHS[a]}-$1
+      docker push standardcore/vscode-golang:${GO_VERSIONS[i]}-${ARCHS[a]}-$1
    done
 done
 
