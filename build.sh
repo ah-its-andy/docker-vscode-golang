42004ec9bb0685960b5b83244eab68b0c6760e05
Andrew <mykge@outlook.com>
2022-06-24 19:20:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-GO_VERSIONS=("go1.16.3" "go1.16.2" "go1.16.1" "go1.16" "go1.15.11" "go1.15.10" "go1.15.9" "go1.15.8" "go1.15.7" "go1.15.6" "go1.15.5" "go1.15.4" "go1.15.3" "go1.15.2" "go1.15.1" "go1.15" "go1.17" "go1.18" )
+GO_VERSIONS=("go1.17.11")
 for((i=0;i<${#GO_VERSIONS[@]};i++));
 do
   echo "========== standardcore/vscode-golang:${GO_VERSIONS[i]}-${VSCODE_GOLANG_CPU_ARCH}-$1 =========="
